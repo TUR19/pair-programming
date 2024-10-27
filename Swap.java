f0560f734b111e19e69b61a6e5e2cbb712859735
XMMM <turarbek191100@gmail.com>
2024-10-28 01:25:12 +0600

    add Swap class and update Trainer class

---
+++ Swap.java
@@ -0,0 +1,67 @@
+import java.util.Date;
+
+public class Swap{
+    private Trainer trainer1;
+    private Trainer trainer2;
+    private Pokemon pokemon1;
+    private Pokemon pokemon2;
+    private Date swapDate;
+    private String swapId;
+
+    public Swap(Trainer trainer1, Pokemon pokemon1, Trainer trainer2, Pokemon pokemon2, String swapId) {
+        this.trainer1 = trainer1;
+        this.pokemon1 = pokemon1;
+        this.trainer2 = trainer2;
+        this.pokemon2 = pokemon2;
+        this.swapId = swapId;
+        this.swapDate = new Date();
+    }
+
+    public void execute(){
+        if (trainer1 == trainer2){
+            System.out.println("ERROR: Trainers should be different.");
+            return;
+        }
+
+        if (!trainer1.getPokemons().contains(pokemon1) || !trainer2.getPokemons().contains(pokemon2)) {
+            System.out.println("ERROR: One of the Pokemon does not belong to its trainer.");
+            return;
+        }
+
+        trainer1.addPokemon(pokemon2);
+        trainer2.addPokemon(pokemon1);
+        trainer1.removePokemon(pokemon1);
+        trainer2.removePokemon(pokemon2);
+
+        System.out.println("The exchange was successfully completed between " + trainer1.getName() + " and " + trainer2.getName());
+        System.out.println("Date of exchange: " + swapDate);
+    }
+
+    public String getSwapId(){
+        return swapId;
+    }
+
+    public Date getSwapDate(){
+        return swapDate;
+    }
+
+    public static void main(String[] args){
+        Trainer Ivan = new Trainer("Ivan");
+        Trainer Alexandr = new Trainer("Alexandr");
+
+        Pokemon charmander = new Pokemon("Charmander", Type.FIRE);
+        Pokemon squirtle = new Pokemon("Squirtle", Type.WATER);
+
+        Ivan.addPokemon(charmander);
+        Alexandr.addPokemon(squirtle);
+
+        Ivan.showTrainerInfo();
+        Alexandr.showTrainerInfo();
+
+        Swap swap = new Swap(Ivan, charmander, Alexandr, squirtle, "swap001");
+        swap.execute();
+
+        Ivan.showTrainerInfo();
+        Alexandr.showTrainerInfo();
+    }
+}
